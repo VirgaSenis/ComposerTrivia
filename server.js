3c7918c5d323fbd2f68d7c48f4b37875809e3054
VirgaSenis <yoonho_choi@hotmail.com>
2017-05-06 21:43:35 -0400

    changed getQuestion method

--- server.js
+++ server.js
@@ -5,92 +5,118 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var CHOICE_CARDINALITY = 6;
-var NUMBER_OF_QUESTIONS = 10;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(session( { secret: 'Something'}));
 app.use(express.static('public'));
 
+var google_drive_url = "https://drive.google.com/uc?id=";
+var number_of_choices = 6;
+var NUMBER_OF_QUESTIONS;
 
-function getComposers() {
-	var data = fs.readFileSync('./public/composers.txt', 'utf8');
-	var list = data.split('\r\n');
-	list.splice(-1, 1); 
-	return list;
+var composers;
+var questions;
+
+function loadData() {
+	loadComposers();
+	loadQuestions();
+	NUMBER_OF_QUESTIONS = questions.length;
 }
-var composers = getComposers();
-var questions = JSON.parse(fs.readFileSync('./public/questions.json', 'utf8'));
 
-app.listen(8080);
+function loadComposers() {
+	var pathname = path.join(__dirname + '/public/composers.txt');
+	var data = fs.readFileSync(pathname, 'utf8');
+	composers = data.split('\r\n');
+	composers.splice(-1, 1); 
+}
 
+function loadQuestions() {
+	var pathname = path.join(__dirname + '/public/questions.json');
+	questions = JSON.parse(fs.readFileSync(pathname, 'utf8'));
+}
+
+function runServer() {
+	app.listen(8080);
+	console.log('listening on 127.0.0.1:8080');
+}
+
+loadData();
+runServer();
 
 app.get('/', function(req, res) {
 	console.log('request for / receieved.');
 	res.sendFile( path.join(__dirname + '/public/home.html') );
+
 });
 
-app.get('/init', function(req, res) {
+app.post('/init', function(req, res) {
 	console.log('request for /init receieved.');
 
 	req.session.questionNumber = 1;
 	req.session.score = 0;
+
 	res.send('ok');
 });
 
 app.get('/question', function(req, res) {
 	console.log('request for GET /question receieved.');
-	console.log(req.session);
+	var session = req.session;
 
-	var data = {};
-	data.score = req.session.score;
-	data.numberOfQuestions = NUMBER_OF_QUESTIONS;
 
-	console.log(questions);
-
-	var questionIndex = req.session.questionNumber - 1;
-	if (questionIndex == NUMBER_OF_QUESTIONS) {
-		data.isFinished = true;
-
-		res.send(data);
-
+	if (session.questionNumber >= NUMBER_OF_QUESTIONS) {
+		console.log('game finished');
+		res.status(302).send({ redirect: '#/end'});
 	} else {
-		data.url = questions[questionIndex].link;
-		data.choices = makeQuestionChoices(questionIndex);
+		var qIndex = session.questionNumber - 1;
+		var data = {};
+		data.score = session.score + " / " + qIndex;
+		data.questionNumber = session.questionNumber;
+		data.url = google_drive_url + questions[qIndex].id;
+		data.choices = getQuestionChoices(qIndex);
+
 		res.send(data);
 	}
 
 	res.end();
 }); 
 
-function makeQuestionChoices(questionIndex) {
+function getQuestionChoices(idx) {
 	var choices = [];
-	insertAnswer(choices, questionIndex);
-	insertWrongChoices(choices);
-	return choices;
-}
+	var correctChoice = questions[idx].composer;
+	
+	// remove correct choice from the composer list
+	removeElement(composers, correctChoice);
 
-function insertAnswer(choices, questionIndex) {
-	var answer = questions[questionIndex].composer;
-	questionIndex = Math.round(Math.random() * (CHOICE_CARDINALITY-1) );
-	choices[questionIndex] = answer;
-}
-
-function insertWrongChoices(choices) {
-	for (var i=0; i<CHOICE_CARDINALITY; i++) {
-		while (choices[i] === undefined || choices[i] === null) {
-			var j = Math.round(Math.random() * composers.length);
-			var composer = composers[j];
-
-			if (choices.indexOf(composer) == -1) {
-				choices[i] = composers[j];
-			}
-		} 
+	// add random composers to the choices, while removing them from 
+	// composer list
+	for (var i=0; i<number_of_choices; i++) {
+		var idx = Math.floor(Math.random() * composers.length);
+		choices[i] = composers[idx];
+		removeElement(composers, choices[i]);
 	}
 
+	// add correct choice at random index
+	addAtRandomIndex(choices, correctChoice);
+
+	// re-append removed composers
+	composers.concat(choices); 
+
 	return choices;
 }
+
+function removeElement(list, item) {
+	var idx = list.indexOf(item);
+	if (idx != -1) {
+		list.splice(idx, 1);
+	}
+}
+
+function addAtRandomIndex(list, item) {
+	var idx = Math.floor(Math.random()*list.length);
+	list[idx] = item;
+}
+
 
 app.post('/response', function(req, res) {
 	console.log('request for POST /answer received.');
